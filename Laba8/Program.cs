53c07aac741b5d6a93c02673857ab80599cbd78f
Valeria Kosilo <valeria.kosilo@mail.ru>
2020-12-05 18:26:58 +0300

    Labs

---
+++ Laba8/Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Laba8
+{
+    class OutOfRange : Exception //исключение
+    {
+        public OutOfRange(string message) : base(message) { }
+    }
+    interface IAll<T> //обощенный интерфейс
+    {
+        void AddElem(T elem);
+
+        void Delete(T elem);
+
+        void ShowElem();
+    }
+
+    public class Good { }
+    public class Example<T> where T : Good { }
+    //должен быть классом Good или его наследником
+
+    public class CollectionType<T> : IAll<T>
+    {
+        List<T> str = new List<T>(); 
+
+        //Добавление элемента в конец
+        public void AddElem(T elem)
+        {
+            str.Add(elem);
+        }
+
+        //Удаление эл-та по заданному индексу
+        public void DeleteElem(int index)
+        {
+            if (index > str.Count())//!!!!!!!!!!!!!!!!!!!
+                throw new OutOfRange("Такого индекса нет!");
+            else
+                str.RemoveAt(index);
+        }
+
+        //Удаление зданного эл-та 
+        public void Delete(T elem)
+        {
+            str.Remove(elem);
+        }
+
+        //Вывод всех элементов
+        public void ShowElem()
+        {
+            foreach (T x in str)
+            {
+                Console.WriteLine($"{x} ");
+            }
+        }
+        //получение эл-та по индексу
+        public T Elements(int i)
+        {
+            return str[i];
+        }
+        //кол-во эл-тов
+        public int Count()
+        {
+            return str.Count();
+        }
+
+        //проверка на равенство
+        public static bool operator ==(CollectionType<T> str1, CollectionType<T> str2)
+        {
+            int x = 0;
+            if (str1.Count() != str2.Count())
+                return false;
+            else
+            {
+                for (int i = 0; i < str1.Count(); i++)
+                {
+                    if (str1.str.Contains(str2.Elements(i)))
+                        x++;
+                }
+                if (x == str1.Count())
+                    return true;
+                else
+                    return false;
+            }
+        }
+        //проверска на неравенство
+        public static bool operator !=(CollectionType<T> str1, CollectionType<T> str2)
+        {
+            int x = 0;
+            if (str1.Count() != str2.Count())
+                return true;
+            else
+            {
+                for (int i = 0; i < str1.Count(); i++)
+                {
+                    if (str1.str.Contains(str2.Elements(i)))
+                        x++;
+                }
+                if (x == str1.Count())
+                    return false;
+                else
+                    return true;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != this.GetType()) return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            Random random = new Random();
+            int hash = random.Next(516, 531);
+            hash = 217 + (hash * 3);
+            return hash;
+        }
+
+        //проверка, пустой ли список
+        public static bool operator true(CollectionType<T> str1)
+        {
+            int x = str1.Count();
+            if (x == 0)
+                return true;
+            else
+                return false;
+        }
+        //проверка, непустой ли список
+        public static bool operator false(CollectionType<T> str1)
+        {
+            int x = str1.Count();
+            if (x == 0)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    abstract class Goods
+    {
+        public double price { get; set; }
+        public string date { get; set; }
+        public string productname { get; set; }
+    }
+    class Product : Goods
+    {
+        public Product(double Price, string Date, string ProductName)
+        {
+            price = Price;
+            date = Date;
+            productname = ProductName;
+        }
+        public override string ToString()
+        {
+            return "Название продукта: " + productname + " Цена: " + price;
+        }
+    }
+
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            CollectionType<int> num1 = new CollectionType<int>();
+            num1.AddElem(1);
+            num1.AddElem(2);
+            num1.AddElem(3);
+
+            Console.WriteLine("Вывод элементов списка:");
+            num1.ShowElem();
+            Console.WriteLine();
+
+            Console.WriteLine("Удаление элемента списка - 3:");
+            num1.Delete(3);
+            num1.ShowElem();
+
+            CollectionType<string> num2 = new CollectionType<string>();
+            num2.AddElem("1");
+            num2.AddElem("2");
+            num2.AddElem("3");
+            Console.WriteLine();
+
+
+            Console.WriteLine("Вывод элементов списка:");
+            num2.ShowElem();
+            Console.WriteLine();
+
+            Console.WriteLine("Удаление элемента списка - 3:");
+            num2.Delete("3");
+            num2.ShowElem();
+            Console.WriteLine();
+
+            CollectionType<Product> num3 = new CollectionType<Product>();
+            Product num4 = new Product(23.99, "23.11.2020", "Cake");
+            Product num5 = new Product(25.9, "20.11.2020", "Clock");
+            num3.AddElem(num4);
+            num3.AddElem(num5);
+            Console.WriteLine("Вывод элементов списка:");
+            num3.ShowElem();
+            Console.WriteLine();
+
+            Console.WriteLine("Проверка на равенство 2 списков");
+            if(num4 == num5)
+                Console.WriteLine("Равны");
+            else
+                Console.WriteLine("Не равны");
+            Console.WriteLine();
+
+            Console.WriteLine("Проверка пустой список или нет");
+            if (num1)
+                Console.WriteLine("Список пуст");
+            else
+                Console.WriteLine("Список не является пустым");
+            Console.WriteLine();
+
+
+            try
+            {
+                num3.DeleteElem(5);
+            }
+
+            catch(OutOfRange ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Конец!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laba9/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba9
+{
+    class Programmer
+    {
+
+        List<string> proger = new List<string>();
+
+        public delegate void Example(string mes);
+        public event Example Delete;
+        public event Example ChangeLevel;
+
+        public void AddElem(string item)
+        {
+            proger.Add(item);
+        }
+
+        public void InsertElem(int index, string item)
+        {
+            proger.Insert(index, item);
+        }
+
+        //Удаление эл-та из списка по заданному индексу
+        public void RemoveElem(int index)
+        {
+            proger.RemoveAt(index);
+        }
+
+        //Вывод всех элементов списка
+        public void Show()
+        {
+            foreach (string x in proger)
+            {
+                Console.WriteLine($"{x} ");
+            }
+        }
+        //получение эл-та по индексу
+        public string Elements(int i)
+        {
+            return proger[i];
+        }
+        //кол-во эл-тов в списке
+        public int Count()
+        {
+            return proger.Count();
+        }
+
+        public void ProgDelete(string Name, Programmer proger)
+        {
+                int p = 0;
+                for (int i = 0; i < proger.Count(); i++)
+                {
+                    if (Name == proger.Elements(i))
+                    {
+                        proger.RemoveElem(i);
+                        p = 1;
+                    }
+                }
+                if (p == 1)
+                {
+                    Delete.Invoke($"Программист { Name } удален из списка!");
+                }
+                else
+                {
+                    Delete.Invoke("Невозможно выполнить операцию!");
+                }
+        }
+
+        public void ProgLevelUp(Programmer proger, int num)
+        {
+            if (num <= proger.Count())
+            {
+                    proger.InsertElem(0, proger.Elements(num));
+                    proger.RemoveElem(num+1);
+
+                    ChangeLevel.Invoke("Порядок списка программистов изменён!");
+            }
+            else
+            {
+                ChangeLevel.Invoke("Невозможно выполнить операцию!");
+            }
+        }
+    }
+        public class Stroke
+        {
+            public static string DoubleSpace(string a)
+            {
+                return a.Replace("  "," ");
+            }
+
+            public static string LowerCase(string a)
+            {
+                return a.ToLower();
+            }
+
+            public static string SubString(string a)
+            {
+                return a.Substring(0, 5);
+            }
+
+            public static string UpperCase(string a)
+            {
+                return a.ToUpper();
+            }
+
+            public static string AddDot(string a)
+            {
+                a += ".";
+                return a;
+            }
+
+        public static void AddSym(string a)
+        {
+            a += "!";
+            Console.WriteLine(a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer spisok1 = new Programmer();
+            spisok1.AddElem("Карина");
+            spisok1.AddElem("Максим");
+            spisok1.AddElem("Егор");
+            spisok1.AddElem("Валера");
+            spisok1.Show();
+
+            spisok1.Delete += mes => Console.WriteLine(mes);// Добавляем обработчик для события
+            spisok1.ChangeLevel += mes => Console.WriteLine(mes);// Добавляем обработчик для события
+            Console.WriteLine();
+
+            Console.WriteLine();
+            spisok1.ProgLevelUp(spisok1, 3);
+            spisok1.ProgDelete("Максим", spisok1);
+            spisok1.Show();
+
+
+            Console.WriteLine();
+            Programmer spisok2 = new Programmer();
+            spisok2.AddElem("Кристина");
+            spisok2.AddElem("Виктор");
+            spisok2.AddElem("Яна");
+            spisok2.AddElem("Оля");
+            spisok2.Show();
+
+            Console.WriteLine();
+            spisok2.Delete += mes => Console.WriteLine(mes);// Добавляем обработчик для события
+            spisok2.ChangeLevel += mes => Console.WriteLine(mes);// Добавляем обработчик для события
+
+            Console.WriteLine();
+            spisok2.ProgDelete("Вероника", spisok2);
+            spisok2.ProgLevelUp(spisok2, 15);
+            spisok2.Show();
+
+            Console.WriteLine();
+            string a = " Это  Последнее  Задание";
+            Console.WriteLine(a);
+            Console.WriteLine();
+
+            //стандартный тип делегата
+            Func<string, string> Str = Stroke.AddDot; // делегат указывает на метод AddDot
+            Console.WriteLine(Str(a));
+
+            Str = Stroke.DoubleSpace;
+            Console.WriteLine(Str(a));
+
+            Str = Stroke.SubString;
+            Console.WriteLine(Str(a));
+
+            Str = Stroke.UpperCase;
+            Console.WriteLine(Str(a));
+
+            Str = Stroke.LowerCase;
+            Console.WriteLine(Str(a));
+
+            Console.WriteLine();
+            Action<string> Str1 = Stroke.AddSym;
+            Str1(a);
+
+            Console.ReadKey();
+
+        }
+    }
+}
