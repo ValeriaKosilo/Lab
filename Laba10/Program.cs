9a193fc56d0d706c0473b83315f023475a9fe277
Valeria Kosilo <valeria.kosilo@mail.ru>
2020-12-12 10:25:13 +0300

    New

---
+++ Laba10/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Laba10
+{
+    public class Student
+    {
+        public string name;
+        public string univ;
+        public int course;
+
+        public Student(string Name, string Univ, int Course)
+        {
+            name = Name;
+            univ = Univ;
+            course = Course;
+        }
+
+        public override string ToString()
+        {
+            return "Студент " + univ + " " + name + " " + course;
+        }
+    }
+
+    abstract class Goods
+    {
+        public int price { get; set; }
+        public string Name { get; set; }
+    }
+    class Product : Goods, IComparable
+    {
+        public Product(int Price, string ProductName)
+        {
+            price = Price;
+            Name = ProductName;
+        }
+        public override string ToString()
+        {
+            return "Название продукта: " + Name + " Цена: " + price;
+        }
+        public int CompareTo(object o)
+        {
+            Product p = o as Product;
+            return this.Name.CompareTo(p.Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------Задание 1---------------------");
+            ArrayList first = new ArrayList();
+
+            //Добавляем 5 случайных чисел
+            Random a = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int number = a.Next(0, 20);
+                first.Add(number);
+            }
+
+            //Добавляем строку
+            string str = "Добавь меня в коллекцию!";
+            first.Add(str);
+
+            //Добавляем объект класса
+            Student numb1 = new Student("BSTU", "Anna", 2);
+            first.Add(numb1);
+
+            Console.WriteLine("Удалить элемент из коллекции, введите индекс элемента");
+            int index = Convert.ToInt32(Console.ReadLine());
+            if (index < first.Count)
+            {
+                first.RemoveAt(index);
+                Console.WriteLine("Элемент удален!");
+            }
+            else
+                Console.WriteLine("Элемента с таким индексом нет!");
+            Console.WriteLine();
+
+            Console.WriteLine("Количество элементов: " + first.Count);
+            foreach (object x in first)
+            {
+                if (x is Student)
+                {
+                    Console.WriteLine(x.ToString());
+                }
+                else
+                    Console.WriteLine(x);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Поиск значения 26: ");
+            if (first.Contains(26))
+                Console.WriteLine("Элемент найден!");
+            else
+                Console.WriteLine("Такого элемента нет!");
+            Console.WriteLine();
+
+
+            Console.WriteLine("-----------------Задание 2---------------------");
+            SortedSet<float> second = new SortedSet<float>();
+            second.Add(3.14F);
+            second.Add(3.15F);
+            second.Add(3.16F);
+            second.Add(3.17F);
+            second.Add(3.18F);
+
+            Console.WriteLine("Количество элементов: ");
+            foreach (object x in second)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            // удаляем элементы 1 и 2
+            second.RemoveWhere(item => item == 3.14F || item == 3.15F);
+            foreach (object x in second)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            second.Add(30.6F);
+            second.Add(31.5F);
+
+            Queue<float> second2 = new Queue<float>();
+            foreach (float x in second)
+            {
+                second2.Enqueue(x);
+            }
+
+            Console.WriteLine("Элементы 2 коллекции: ");
+            foreach (float x in second2)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Поиск значения 30.6F: ");
+            if (second2.Contains(30.6F))
+                Console.WriteLine("Элемент найден!");
+            else
+                Console.WriteLine("Такого элемента нет!");
+            Console.WriteLine();
+
+
+            Console.WriteLine("-----------------Задание 3---------------------");
+
+            Product num1 = new Product(20, "Синия ручка");
+            Product num2 = new Product(25, "Красная ручка");
+            Product num3 = new Product(30, "Зелёная ручка");
+
+            SortedSet<Product> third = new SortedSet<Product>();
+            third.Add(num1);
+            third.Add(num2);
+            third.Add(num3);
+
+            Console.WriteLine("Количество элементов: ");
+            foreach (object x in third)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            third.RemoveWhere(item => item == num1 || item == num2);
+            foreach (object x in third)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            Product num4 = new Product(35, "Желтая ручка");
+            Product num5 = new Product(40, "Черная ручка");
+            third.Add(num4);
+            third.Add(num5);
+
+            Queue<Product> second3 = new Queue<Product>();
+            foreach (Product x in third)
+            {
+                second3.Enqueue(x);
+            }
+
+            Console.WriteLine("Элементы 2 коллекции: ");
+            foreach (Product x in second3)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Поиск значения - черная речка: ");
+            if (second3.Contains(num5))
+                Console.WriteLine("Элемент найден!");
+            else
+                Console.WriteLine("Такого элемента нет!");
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------Задание 4---------------------");
+
+            ObservableCollection<Product> fourth = new ObservableCollection<Product>();
+
+            fourth.CollectionChanged += CollChanged;
+
+            fourth.Add(num1);
+            fourth.Add(num2);
+            fourth.Add(num3);
+
+            fourth.RemoveAt(0);
+
+            void CollChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add: // если добавление
+                        Product add = e.NewItems[0] as Product;
+                        Console.WriteLine($"Добавлен новый объект: {add.Name}");
+                        break;
+                    case NotifyCollectionChangedAction.Remove: // если удаление
+                        Product delete = e.OldItems[0] as Product;
+                        Console.WriteLine($"Удален объект: {delete.Name}");
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Laba11/Program.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba11
+{
+    public class Car
+    {
+        public string brand { get; set; } //марка
+        public int yearofissue { get; set; } //год выпуска
+        public string color { get; set; } //цвет
+        public int price { get; set; } //цена
+
+        public Car(string Brand, string Color, int Price, int yofi)
+        { 
+            brand = Brand;
+            color = Color;
+            price = Price;
+            yearofissue = yofi;
+        }
+
+        public int AgeoftheCar() //возраст машины 
+        {
+            return 2020 - yearofissue;
+        }
+    }
+
+    public class Vector
+    {
+        public int[] vect;
+        public Vector(int[] Vect)
+        {
+            vect = Vect;
+        }
+        public int Length()
+        {
+            return vect.Length;
+        }
+        public void Show()
+        {
+            for (int i = 0; i < vect.GetLength(0); i++)
+            {
+                    Console.Write(vect[i] + " ");
+            }
+        }
+
+        public bool Have()
+        {
+            int kol = 0;
+            for (int i = 0; i < vect.GetLength(0); i++)
+            {
+                if (vect[i] == 0)
+                    kol++;
+            }
+            if (kol > 0)
+                return true;
+            else
+                return false;
+        }
+        public double Modul()
+        {
+            double mod = 0;
+            for (int i = 0; i < vect.GetLength(0); i++)
+            {
+                mod += (vect[i])^2;
+            }
+            mod = Math.Sqrt(mod);
+            return mod;
+        }
+
+        public bool Minus()
+        {
+            double otr = 0;
+            for (int i = 0; i < vect.GetLength(0); i++)
+            {
+                if (vect[i] < 0)
+                    otr++;
+            }
+            if (otr > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+    class Player
+    {
+        public string Name { get; set; }
+        public string Team { get; set; }
+    }
+    class Team
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------1 задание-------------");
+            string[] month = {"January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"};
+            string[] sum_win = { "December", "January", "Ferbruary", "June", "July", "August"};
+            int n = 5;
+
+            IEnumerable<string> first = month.Where(l => l.Length == n);
+            Console.WriteLine("Длина строки равна 5:");
+            foreach (string x1 in first)
+            {
+                Console.WriteLine(x1);
+            }
+            Console.WriteLine();
+
+            IEnumerable<string> second = month.Intersect(sum_win);
+            Console.Write("Зимние и летние месяцы:");
+            foreach (string x2 in second)
+            {
+                Console.WriteLine(x2);
+            }
+            Console.WriteLine();
+
+            IEnumerable<string> third = month.OrderBy(x => x);
+            Console.WriteLine("В алфавитном порядке:");
+            foreach (string x3 in third)
+            {
+                Console.WriteLine(x3);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Содержат букву u, не менее 4 символов:");
+            IEnumerable<string> fourth = from x in month
+                                        where x.Length >= 4 & x.Contains("u")
+                                        select x;
+
+            foreach (string x4 in fourth)
+            {
+                Console.WriteLine(x4);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("----------------2 задание-------------");
+            List<Car> task2 = new List<Car>
+            {
+                new Car("Opel", "Blue", 200 ,2010),
+                new Car("Bentley", "Black", 800 ,2018),
+                new Car("BMW", "Red", 500 ,2001),
+            };
+
+            IEnumerable<Car> car = from i in task2
+                                   orderby i.brand
+                                   select i;
+
+            Console.WriteLine("List содержит: ");
+            foreach (Car i in car)
+            {
+                Console.WriteLine(i.brand);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("----------------3 задание-------------");
+            Vector v1 = new Vector(new int[] {1, 2, 5, 4});
+            Vector v2 = new Vector(new int[] {1, 0, -3});
+            Vector v3 = new Vector(new int[] {-1, 0});
+            Vector v4 = new Vector(new int[] {0, 2, 3, 7, 11});
+            Vector v5 = new Vector(new int[] {3});
+
+            List<Vector> vector = new List<Vector> { };
+            vector.Add(v1);
+            vector.Add(v2);
+            vector.Add(v3);
+            vector.Add(v4);
+            vector.Add(v5);
+
+            var zero_kol = from x in vector
+                           where x.Have()
+                           select x;
+
+            Console.Write("Количество векторов, содержащих 0: " + zero_kol.Count() + "\n");
+            foreach (var x in zero_kol)
+            {
+                x.Show();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("C наим модулем: ");
+            var mod = vector.OrderBy(x => x.Modul());
+            var less_mod = mod.Take(2);
+            foreach (var x in less_mod)
+            {
+                x.Show();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Упорядоченный список по размеру: ");
+            var max = vector.OrderBy(x => x.Length());
+            foreach (var x in max)
+            {
+                x.Show();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Максимальный вектор: ");
+            var max1 = max.Skip(mod.Count() - 1);
+            foreach (var x in max1)
+            {
+                x.Show();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Первый вектор с отр занчением: ");
+            var otr = from x in vector
+                      where x.Minus()
+                      select x;
+
+            var first_otr = otr.Take(1);
+            foreach (var x in first_otr)
+            {
+                x.Show();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Массив векторов длины - 3, 4, 5: ");
+            var massiv = from x in vector
+                      where x.Length() == 3 | x.Length() == 4 | x.Length() == 5
+                      select x;
+
+            foreach (var x in massiv)
+            {
+                x.Show();
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("----------------4 задание-------------");
+
+            string[] tas4 = { "October", "September", "May"};
+            IEnumerable<string> task4 = from x in month
+                                        .Where(f => f.Contains("a"))//квантор и уловие
+                                        .Take(3) //разбиение
+                                        .OrderBy(f => f) //упорядочивание
+                                        .Except(tas4) //множество
+                                        select x;
+
+            foreach (string i in task4)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("----------------5 задание-------------");
+
+            List<Team> teams = new List<Team>()
+            {
+                new Team { Name = "Бавария", Country ="Германия" },
+                new Team { Name = "Барселона", Country ="Испания" }
+            };
+            List<Player> players = new List<Player>()
+            {
+                new Player {Name="Месси", Team="Барселона"},
+                new Player {Name="Неймар", Team="Барселона"},
+                new Player {Name="Роббен", Team="Бавария"}
+            };
+
+            var result = from pl in players
+                         join t in teams on pl.Team equals t.Name //общий критерий - название команды
+                         select new { Name = pl.Name, Team = pl.Team, Country = t.Country };
+
+            foreach (var item in result)
+                Console.WriteLine($"{item.Name} - {item.Team} ({item.Country})");
+            Console.ReadLine();
+        }
+    }
+}
